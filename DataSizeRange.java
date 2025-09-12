d28aa61d66144ca098c281565c85ba7d9b04a4fe
Prakash Singh Basnal <prakash0basnal0@gmail.com>
2025-09-12 22:55:34 +0530

    Add .gitignore to exclude .class files and implement DataSizeRange class for displaying data type sizes

---
+++ DataSizeRange.java
@@ -0,0 +1,15 @@
+import java.lang.*;
+
+class DataSizeRange {
+    public static void main(String arg[]) {
+        System.out.println("Int min value - " + Integer.MIN_VALUE);
+        System.out.println("Int max value - " + Integer.MAX_VALUE);
+        System.out.println("Int byte - " + Integer.BYTES);
+
+        System.out.println("Short min value - " + Short.MIN_VALUE);
+        System.out.println("Short max value - " + Short.MAX_VALUE);
+        System.out.println("Short byte - " + Short.BYTES);
+
+        System.out.println("Char byte - " + Character.BYTES);
+    }
+}
