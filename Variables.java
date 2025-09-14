20d882dfb3c9c03d874e2edff1aced7cfe8e46ce
Prakash Singh Basnal <prakash0basnal0@gmail.com>
2025-09-14 22:47:52 +0530

    Add Variables class with main method for demonstrating variable types and outputs

---
+++ Variables.java
@@ -0,0 +1,21 @@
+import java.lang.*;
+
+class Variables {
+
+    public static void main(String[] args) {
+        byte b = 5;
+        b = 25;
+        short s = 300;
+        int i = 1000;
+
+        float f = 25.3f;
+
+        char c = 'A';
+
+        System.out.println("Byte - " + b);
+        System.out.println("Short - " + s);
+        System.out.println("Float - " + f);
+        System.out.println("Int - " + i);
+        System.out.println("Char - " + c);
+    }
+}
